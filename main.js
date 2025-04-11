3798e190c2f530db4cb67f99cda8f7228865e136
myyonop <jeunsu813@gmail.com>
2025-04-12 00:49:45 +0900

    2차차

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector(".navbar_btn");
 const menu = document.querySelector(".navbar_menu");
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
     menu.classList.toggle("active");
 });
