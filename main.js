4049f6666e32ad1c74eee236be96be0ceb120e26
myyonop <jeunsu813@gmail.com>
2025-06-16 04:07:31 +0900

    끝

--- main.js
+++ main.js
@@ -21,32 +21,87 @@ window.addEventListener("resize", () => {
 
 // ------------------------------------
 
-// ------------------------------------
+//fade-in
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeEls = document.querySelectorAll("#aboutme .fade-in, #contact .fade-in, #work .fade-in, #team .fade-in");
 
-// 자격증란
-const tabs = document.querySelectorAll('.year-tabs span');
-const certLists = document.querySelectorAll('.cert-list');
-
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-      tabs.forEach(t => t.classList.remove('active'));
-      tab.classList.add('active');
-  
-      const year = tab.dataset.year;
-  
-      // 모든 자격증 리스트 숨기기
-      certLists.forEach(list => {
-        list.style.display = 'none';
-        list.classList.remove('active');
-      });
-  
-      // 선택된 연도만 보여주기기
-      const activeList = document.getElementById(`cert-${year}`);
-      if (activeList) {
-        activeList.style.display = 'flex';
-        setTimeout(() => {
-          activeList.classList.add('active');
-        }, 10);
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible"); // 다시 사라지게 함
       }
     });
+  }, {
+    threshold: 0.1,
   });
+
+  fadeEls.forEach((el) => observer.observe(el));
+});
+
+
+
+// ------------------------------------
+
+//modal
+const $btnOpen = document.querySelectorAll(".btn-open");
+const $modalBg = document.querySelectorAll(".modal-background");
+const $btnClose = document.querySelectorAll(".btn-close");
+
+$btnOpen.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    $modalBg[i].style.display = "flex";
+    document.body.style.overflow = "hidden";
+  });
+});
+
+$btnClose.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    $modalBg[i].style.display = "none";
+    document.body.style.overflow = "unset";
+  });
+});
+
+
+//modal - musical slide
+let imgIndex = 0;
+let position = 0;
+const IMG_WIDTH = 300;
+const $btnPrev = document.querySelector(".btn-prev");
+const $btnNext = document.querySelector(".btn-next");
+const $slideImgs = document.querySelector(".slide-images");
+
+let prev = function () {
+  if (imgIndex > 0) {
+    $btnNext.removeAttribute("disabled");
+    position += IMG_WIDTH;
+    $slideImgs.style.transform = `translateX(${position}px)`;
+    imgIndex = imgIndex - 1;
+  }
+  if (imgIndex == 0) {
+    $btnPrev.setAttribute("disabled", "true");
+  }
+};
+
+let next = function () {
+  if (imgIndex < 6) {
+    $btnPrev.removeAttribute("disabled");
+    position -= IMG_WIDTH;
+    $slideImgs.style.transform = `translateX(${position}px)`;
+    $slideImgs.style.transition = "transform .5s ease-out";
+    imgIndex = imgIndex + 1;
+  }
+  if (imgIndex == 5) {
+    $btnNext.setAttribute("disabled", "true");
+  }
+};
+
+let init = function () {
+  $btnPrev.setAttribute("disabled", "true");
+  $btnPrev.addEventListener("click", prev);
+  $btnNext.addEventListener("click", next);
+};
+
+init();
+
